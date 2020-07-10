7a7b9d269cfeec01d8b674d20c0c8f06e75c09f6
zyz <zyz951123@gmail.com>
2020-07-09 21:11:33 -0400

    assignment 1

---
+++ React/confusion/src/components/DishDetailComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import {Card, CardBody, CardTitle, CardText, CardImg} from 'reactstrap';
+
+class DishDetail extends Component {
+
+    renderDish(dish) {
+        if (dish != null) {
+          return(
+            <Card className="col-12 col-md-5">
+              <CardImg width='100%' object src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          );
+        }
+        else {
+          return(
+            <div></div>
+          );
+        }
+      }
+    
+    renderComment(comments) {
+        if (comments == null) {
+            return (<div></div>)
+        }
+
+
+        //list items
+        const commentList = comments.map((cmnt)=> {
+          return(
+            <li key={cmnt.id}>
+              <p>{cmnt.comment}</p>
+              <p>-- {cmnt.author},
+              &nbsp;
+              {new Intl.DateTimeFormat('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: '2-digit'
+                  }).format(new Date(cmnt.date))}
+              </p>
+            </li>
+          );
+        });
+        
+        return(
+            <div className="col-12 col-md-5">
+                <h3>Comment</h3>
+                <ul className="list-unstyled">{commentList}</ul>
+            </div>
+        );
+        
+    }
+
+    render() {
+        const dish = this.props.dish;
+        if (dish == null) {
+          return (<div></div>)
+      }
+
+        const dishItem = this.renderDish(dish);
+        const commentItem = this.renderComment(dish.comments);
+        return(
+            <div className="row">
+                {dishItem}
+                {commentItem}
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -7,31 +8,17 @@ class Menu extends Component {
         this.state={
           selectedDish: null
         };
+        // console.log('Menu component constructor is called');
     }
+
+    // componentDidMount() {
+    //   console.log('Menu component componentDidMount is called');
+    // }
 
     onDishSelect(dish) {
       this.setState({
         selectedDish: dish
       });
-    }
-
-    renderDish(dish) {
-      if (dish != null) {
-        return(
-          <Card>
-            <CardImg width='100%' object src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle heading>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        );
-      }
-      else {
-        return(
-          <div></div>
-        );
-      }
     }
 
     render() {
@@ -48,14 +35,14 @@ class Menu extends Component {
             );
         });
 
+        // console.log('Menu component render is called');
+
         return (
           <div className="container">
             <div className="row">
               {menu}          
             </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
-            </div>
+            <DishDetail dish={this.state.selectedDish}></DishDetail>
           </div>
         );
     }
